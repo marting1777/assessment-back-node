98e09a242591e524e8c7d4bde2a24245efe6b06d
marting1777 <martin-galli@outlook.com>
2018-12-27 10:41:14 -0300

    EmployeeProvider + Debugging

--- app.js
+++ app.js
@@ -5,12 +5,11 @@ const routes = require('./routes')
 const user = require('./routes/user')
 const http = require('http')
 const path = require('path')
+const EmployeeProvider = require('./employeeProvider').EmployeeProvider
 
 const port = process.env.PORT || 3000
 
 const app = express()
-
-app.use(express.static(path.join(__dirname, '/public')))
 
 // GET Clients List With Any Role
 app.get('/getClientList', (req, res) => {
@@ -69,6 +68,15 @@ app.get('/getClientListById/:id', (req, res) => {
     request(options, (error, response, body) => {
         if (error) {
             return console.log('Unable to handle request', error)
+        } else {
+            if (body) {
+                let clients = JSON.parse(body)
+                tempClient = clients['clients'].filter((client) => {
+                    if (client.id === id) {
+                        return res.json(client)
+                    }
+                })
+            }
         }
     })
 })
@@ -110,6 +118,52 @@ app.get('/getPoliciesByName/:name', (req, res) => {
                                         }
                                     })
                                     return res.json(PolicyList)
+                                }
+                            }
+                        })
+                    }
+                })
+            }
+        }
+    })
+})
+
+// GET User With Its Proper Policy
+app.get('/getUserByPolicy/:id', (req, res) => {
+    let policyId = req.params.id
+    let url = 'http://mocky.io/v2/580891a4100000e8242b75c5'
+    let options = {
+        url: url,
+        method: 'GET',
+        strictSSL: false
+    }
+    request(options, (error, response, body) => {
+        if (error) {
+            return console.log('Unable to handle request', error)
+        } else {
+            if (body) {
+                console.log('Body:', body)
+                let policies = JSON.parse(body)
+                tempClient = policies['policies'].filter((policy) => {
+                    if (policy.id === policyId) {
+                        let clientId = policy.clientId
+                        let url = 'http://mocky.io/v2/5808862710000087232b75ac'
+                        let options = {
+                            url: url,
+                            method: 'GET',
+                            strictSSL: false
+                        }
+                        request(options, (error, response, body) => {
+                            if (error) {
+                                return console.log('Unable to handle request', error)
+                            } else {
+                                if (body) {
+                                    let clients = JSON.parse(body)
+                                    tempClient = clients['clients'].filter((client) => {
+                                        if (client.id === clientId) {
+                                            return res.json(client)
+                                        }
+                                    })
                                 }
                             }
                         })
