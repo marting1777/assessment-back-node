cfe50650e598497e62b8963e368b9aed95de8bac
marting1777 <martin-galli@outlook.com>
2018-12-26 22:20:53 -0300

    GET Clients List + GET Client List By Name

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // Module Dependencies
 const express = require('express')
+const request = require('request')
 const routes = require('./routes')
 const user = require('./routes/user')
 const http = require('http')
@@ -8,6 +9,53 @@ const path = require('path')
 const port = process.env.PORT || 3000
 
 const app = express()
+
+app.use(express.static(path.join(__dirname, '/public')))
+
+// GET Employee List With Any Role
+app.get('/getClientList', (req, res) => {
+    let url = 'http://mocky.io/v2/5808862710000087232b75ac';
+    let options = {
+        url: url,
+        method: 'GET',
+        strictSSL: false
+    };
+    request(options, (error, response, body) => {
+            if (error) {
+                return console.error('Unable to handle request', error)
+            } else {
+                if (body) {
+                    return res.json(JSON.parse(body))
+                }
+            }
+        }
+    )
+})
+
+app.get('/getClientListByName/:name', (req, res) => {
+    let name = req.params.name
+    let url = 'http://mocky.io/v2/5808862710000087232b75ac'
+    let options = {
+        url: url,
+        method: 'GET',
+        strictSSL: false
+    }
+    request(options, (error, response, body) => {
+        if (error) {
+            return console.log('Unable to handle request', error)
+        } else {
+            if (body) {
+                let clients = JSON.parse(body)
+                tempClient = clients['clients'].filter((client) => {
+                    if (client.name === name) {
+                        return res.json(client)
+                    }
+                })
+            }
+        }
+    })
+})
+
 
 app.listen(port, () => console.log(`Starting on port ${port}`))
 
